7025441a7894c225f6ca63e778e60628e23f1088
Sven <sven.krusch@web.de>
2022-02-21 15:21:12 +0100

    14.02 Commit für C# Binary Tree Forest Klasse

---
+++ BinaryTrees/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        public int value;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            value = item;
+            left = right = null;
+        }
+
+    }
+
+    public class Forest
+    {
+        private Node root = null;
+        public Forest()
+        {
+            root = null;
+        }
+
+        public void insert(int key)
+        {
+            root = insertRec(root, key);
+        }
+
+        private Node insertRec(Node root, int key)
+        {
+            if (root == null)
+            {
+                root = new Node(key);
+                return root;
+            }
+
+
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
